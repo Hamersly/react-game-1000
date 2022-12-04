6bdbd75fa506138ecf6533cab60e6766cb1e79da
Hamersly <maksimwanderer96@gmail.com>
2022-12-05 01:50:02 +0500

    add helpers

--- src/components/ButtonBlock/ButtonBlock.tsx
+++ src/components/ButtonBlock/ButtonBlock.tsx
@@ -3,79 +3,11 @@ import {ButtonBlockDiv, PassButton, PlayButton} from "./ButtonBlock.styled";
 import {useAppDispatch, useAppSelector} from "../../store/hooks";
 import {addDiceCheck, addDicesMeanings, addUserCheck, diceCheckZero} from "../../store/gameLayer/slice";
 import {getDiceCheck} from "../../store/gameLayer/selectors";
+import {countingDiceCheck, generateDicesMeanings} from "../../helpers/helpers";
 
 export const ButtonBlock: FC = () => {
   const diceCheckStore: number = useAppSelector(getDiceCheck)
   const dispatch = useAppDispatch();
-
-  const generateDicesMeanings = (): number[] => {
-    const meanings = []
-    const min = Math.ceil(1);
-    const max = Math.floor(7);
-
-    for (let i = 0; i < 5; i++) {
-      const meaning = Math.floor(Math.random() * (max - min)) + min
-      meanings.push(meaning)
-    }
-    console.log(meanings)
-    return meanings
-  }
-
-  const countingDiceCheck = (meanings: number[] = []): number => {
-
-    if (meanings) {
-      let counter: number = 0
-      const countItems: any = {}
-
-      for (let i = 1; i < 7; i++) {
-        countItems[i] = meanings.filter(meaning => meaning === i).length
-      }
-
-      if (
-        countItems[1] === 1 &&
-        countItems[2] === 1 &&
-        countItems[3] === 1 &&
-        countItems[4] === 1 &&
-        countItems[5] === 1
-      ) {
-        counter += 125
-        return counter
-      } else if (
-        countItems[2] === 1 &&
-        countItems[3] === 1 &&
-        countItems[4] === 1 &&
-        countItems[5] === 1 &&
-        countItems[6] === 1
-      ) {
-        counter += 250
-        return counter
-      }
-
-      for (const key in countItems) {
-        if (key === '1') {
-          if (countItems[key] === 1) counter += 10
-          else if (countItems[key] === 2) counter += 20
-          else if (countItems[key] === 3) counter += 100
-          else if (countItems[key] === 4) counter += 200
-          else if (countItems[key] === 5) counter += 1000
-        } else if (key === '5') {
-          if (countItems[key] === 1) counter += 5
-          else if (countItems[key] === 2) counter += 10
-          else if (countItems[key] === 3) counter += 50
-          else if (countItems[key] === 4) counter += 100
-          else if (countItems[key] === 5) counter += 500
-        } else {
-          if (countItems[key] === 3) counter += Number(key) * 10
-          else if (countItems[key] === 4) counter += Number(key) * 20
-          else if (countItems[key] === 5) counter += Number(key) * 100
-        }
-      }
-
-      console.log(counter)
-      return counter
-
-    } else return 0
-  }
 
   const handleClickPlay = (event: MouseEvent<HTMLElement>) => {
     const meanings = generateDicesMeanings()
@@ -98,3 +30,4 @@ export const ButtonBlock: FC = () => {
     </ButtonBlockDiv>
   );
 };
+
